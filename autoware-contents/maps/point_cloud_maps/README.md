2cf2df37676cfad962e613109047d334433c1e52
hatem-darweesh <hatem.darweesh@gmail.com>
2022-01-25 00:42:21 +0900

    First commit for OpenPlanner Agent for CARLA Simulator

---
+++ autoware-contents/maps/point_cloud_maps/README.md
@@ -0,0 +1,7 @@
+# PointCloud maps for ROS / Autoware usage
+
+This folder contains PointCloud Maps for the various towns provided with CARLA.
+
+The PointCloud files are using a right-handed coordinate system like ROS / Autoware.
+Therefore the y axis is negated in comparison to native CARLA coordinates.
+
---
+++ start.sh
@@ -0,0 +1,24 @@
+#! /bin/bash -e
+role_name=$1
+map_name=$2
+
+echo "-------------------------------"
+echo "Starting OpenPlanner .. "
+echo $role_name
+echo $map_name
+echo "-------------------------------"
+
+## Map conversion from OpenDRIVE to openpanner .kml map format 
+# cd ${TEAM_CODE_ROOT}/assuremappingtools/bin
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:libs
+# ./assure_map_editor -kml ${TEAM_CODE_ROOT}/hdmaps/$map_name.xodr ${TEAM_CODE_ROOT}/hdmaps/$map_name.kml -res 1.0 -lanechange
+# cd ${LEADERBOARD_ROOT}
+
+# source the autoware that will be used as autonomous agent
+source /home/hatem/carla-0.9.13/autoware.ai.openplanner/devel/setup.bash
+
+# launch the ROS topics conversion and start autoware 
+roslaunch ${TEAM_CODE_ROOT}/bridge2autoware.launch role_name:=$role_name map_name:=$map_name
+
+
+
