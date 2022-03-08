c27a50b5442f0acb63120581609b7fed2b01b2f9
hatem-darweesh <hatem.darweesh@gmail.com>
2022-03-08 15:53:44 +0900

    Improve configuration flow when starting different simulation options
    Add point cloud filtering to euclidean cluster, autoware.1.15 rayground removal has an issue with distance filtering

--- start.sh
+++ start.sh
@@ -1,11 +1,15 @@
 #! /bin/bash -e
 role_name=$1
 map_name=$2
+explore_mode=$3
+route_topic=$4
 
 echo "-------------------------------"
 echo "Starting OpenPlanner .. "
 echo $role_name
 echo $map_name
+echo $explore_mode
+echo $route_topic
 echo "-------------------------------"
 
 ## Map conversion from OpenDRIVE to openpanner .kml map format 
@@ -18,7 +22,7 @@ echo "-------------------------------"
 source /home/hatem/carla-0.9.13/autoware.ai.openplanner/devel/setup.bash
 
 # launch the ROS topics conversion and start autoware 
-roslaunch ${TEAM_CODE_ROOT}/bridge2autoware.launch role_name:=$role_name map_name:=$map_name
+roslaunch ${TEAM_CODE_ROOT}/bridge2autoware.launch role_name:=$role_name map_name:=$map_name external_route_topic:=$route_topic explore_mode:=$explore_mode
 
 
 
