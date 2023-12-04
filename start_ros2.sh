16b73c9ffdeb2d8fd103248ad5fcee505d74e178
hatem-darweesh <hatem.darweesh@gmail.com>
2023-12-04 22:53:02 +0900

    Supporting ROS2-Humbler and Ubuntu 22.04

--- start_ros2.sh
+++ start_ros2.sh
@@ -12,8 +12,6 @@ echo $explore_mode
 echo $route_topic
 echo "-------------------------------"
 
-source /home/hatem/hatem-repos/autoware-universe/autoware/install/setup.bash
-
 #ros2 launch ${TEAM_CODE_ROOT}/autoware_launch/universe.launch.xml map_path:=/home/hatem/Data/autoware_uni_sample_data/${map_name} vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit
-ros2 launch /home/hatem/hatem-repos/autoware-universe/autoware/src/launcher/autoware_launch/autoware_launch/launch/autoware.launch.xml map_path:=/home/hatem/Data/autoware_uni_sample_data/${map_name} vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit
-#$SHELL
+# ros2 launch /home/hatem/hatem-repos/autoware/autoware/src/launcher/autoware_launch/autoware_launch/launch/autoware.launch.xml map_path:=/home/hatem/Data/autoware_uni_sample_data/${map_name} vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit
+$SHELL
