2e40c3a89448956372c1d386013656f89e706c0a
hatem-darweesh <hatem.darweesh@gmail.com>
2024-03-05 20:58:34 +0900

    Fix rus_srunner script and test the bridge with scenario runner

--- start_ros2.sh
+++ start_ros2.sh
@@ -4,13 +4,17 @@ map_name=$2
 explore_mode=$3
 route_topic=$4
 
+echo "carla_release"
 echo "-------------------------------"
 echo "Starting OpenPlanner .. "
-echo $role_name
-echo $map_name
-echo $explore_mode
-echo $route_topic
 echo "-------------------------------"
+echo "Agent Name: " $role_name
+echo "Exploration Mode: " $explore_mode
+# echo $route_topic
+echo "Map Path: " ${OP_AGENT_ROOT}/autoware-contents/maps/$map_name
+echo "CARLA Path: " ${CARLA_ROOT}
+echo "-------------------------------"
+echo ""
 
 # ros2 launch /home/hatem/carla/op_agent/autoware_carla_launch/carla_autoware_sensors_interface.xml
 ros2 launch ${OP_AGENT_ROOT}/autoware_carla_launch/carla_simulator.launch.xml map_path:=${OP_AGENT_ROOT}/autoware-contents/maps/$map_name vehicle_model:=sample_vehicle sensor_model:=carla_sensor_kit
